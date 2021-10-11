dd9ca3cc119ad38bc2a7dbe0dd2d9c0b9fd3c7cf
Hiroshi Tachibana <tachibana@minden.co.jp>
2021-10-11 09:51:11 +0900

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi World!'));
+app.get('/', (req, res) => {
+    res.send('Another World!');
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
